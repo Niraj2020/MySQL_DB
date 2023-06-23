db827d389e93322881f0f3397d93aaf88b602e20
Neeraj_K_Sharma_08 <61144634+Niraj2020@users.noreply.github.com>
2023-06-23 11:14:43 +0530

    Add files via upload

---
+++ SQL_workbench.sql
@@ -0,0 +1,9 @@
+use testineuron;
+show databases;
+show tables;
+insert into testineuron.glassdata values(24, 1.51751, 12.81, 3.57, 1.35, 73.02, 0.62, 8.59, 0.0, 0.0, 1);
+insert into testineuron.glassdata values(22, 1.51966, 14.77, 3.75, 0.29, 72.02, 0.03, 9.0, 0.0, 0.0, 1);
+insert into testineuron.glassdata values(21, 1.5175, 12.82, 3.55, 1.49, 72.75, 0.54, 8.52, 0.0, 0.19, 1);
+insert into testineuron.glassdata values(18, 1.52196, 14.36, 3.85, 0.89, 71.36, 0.15, 9.15, 0.0, 0.0, 1);
+insert into testineuron.glassdata values(15, 1.51763, 12.61, 3.59, 1.31, 73.29, 0.58, 8.5, 0.0, 0.0, 1);
+select*from testineuron.glassdata;
